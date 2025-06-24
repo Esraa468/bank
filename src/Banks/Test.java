c979a4e89963a81bb708401b12d6b4f633026986
Esraa468 <asraabwshywn9@gmail.com>
2025-06-24 12:09:11 +0300

    Add files via upload
    des

---
+++ src/Banks/Test.java
@@ -0,0 +1,15 @@
+package Banks;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+    Scanner input= new Scanner(System.in);
+    bankAccount B=new bankAccount();
+    
+    
+    
+	}
+}
---
+++ src/Banks/bankAccount.java
@@ -0,0 +1,77 @@
+package Banks;
+
+public class bankAccount {
+
+	
+	
+	
+	private String account;
+	 
+	private int password;
+	 
+	private double amount;
+	 
+	private double balance;
+	 
+	 
+	
+	public void setaccount(String account) {
+		this.account=account;
+	}
+	 public String getaccount() {
+		 
+		 return account;
+	 }
+
+public void setpassword(int password) {
+	
+	   this.password=password;
+}
+   public  int getpassword() {
+  return password;
+
+   }
+
+
+
+         public void steamount(double amount) {
+	        this.amount=amount;
+}
+           public void setamount() {
+	
+	          this.amount=amount;
+}
+           
+           
+         public  double getamount() {
+        	 
+        	 
+        	 
+         return amount; 
+}
+         
+         
+         public void  setbalance(double balance) {
+        		
+      	   this.balance=balance;
+      }
+         
+         
+         public  double getbalance() {
+        return balance;
+}
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+}
+
