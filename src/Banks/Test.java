571c7fb1493571ccb39ff4c54781cc13e3995270
Esraa468 <asraabwshywn9@gmail.com>
2025-06-24 12:25:23 +0300

    Update Test.java
    add new comment

--- src/Banks/Test.java
+++ src/Banks/Test.java
@@ -3,13 +3,13 @@ package Banks;
 import java.util.Scanner;
 
 public class Test {
-//hello babe//
+
 	public static void main(String[] args) {
 		
     Scanner input= new Scanner(System.in);
     bankAccount B=new bankAccount();
     
     
-    // hi
+//hello new comment
 	}
 }
